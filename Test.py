e4addad7bfb6ba4c6f959aaaa1eebc6fd795446f
ziwujiang <zwjiang@gmail.com>
2019-02-10 16:13:46 +0800

    First Update

---
+++ Test.py
@@ -0,0 +1,12 @@
+class A:
+    def __init__(self, value1=0, value2=0):
+        self._value1 = value1
+        self.__value2 = value2
+
+    def setValue(self, value1, value2):
+        self._value1 = value1
+        self.__value2 = value2
+
+    def show(self):
+        print(self._value1)
+        print(self.__value2)
